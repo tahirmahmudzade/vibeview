60d3cbeb7a7118d1fc383716d6ce0035a80105bf
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-12-17 21:10:46 +0100

    fix(image): fix remote image fetching for image component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.scdn.co", pathname: "/image/**" },
+    ],
+  },
 };
 
 export default nextConfig;
