7ada24745e6f09875c52eb609ced8fe7585615b6
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-12-17 21:12:27 +0100

    feat(main): update index page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,19 @@ import HeroImage from "@/components/HeroImage";
 
 export default function Home() {
   return (
-    <div className="relative bg-gradient-to-r from-green-500 to-black h-screen flex flex-col gap-y-8 lg:gap-y-0 lg:flex-row">
-      <div className="px-6 sm:px-8 sm:py-12 xs:px-8 xs:py-12 lg:px-12 flex flex-col justify-center lg:w-1/2">
-        <div className="mx-auto max-w-lg lg:mx-0">
+    <div className="relative h-screen flex flex-col lg:flex-row bg-gradient-to-r from-green-800 via-green-900 to-black">
+      <div className="flex flex-col justify-center px-6 sm:px-8 xs:px-8 lg:px-12 py-12 lg:w-1/2">
+        <div className="mx-auto max-w-lg lg:mx-0 space-y-6">
           <Logo />
-
           <Header />
-          <div className="mt-6 flex items-center gap-x-6">
+
+          <div className="mt-8 flex items-center gap-x-6">
             <ButtonActions />
           </div>
         </div>
       </div>
 
+      {/* Hero Image Section */}
       <HeroImage />
     </div>
   );
