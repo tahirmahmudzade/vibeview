1c1c13fad4f9d8acc857c399562cd0a4c3c719a4
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-12-18 23:47:41 +0100

    fix: index page scroll issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import HeroImage from "@/components/HeroImage";
 
 export default function Home() {
   return (
-    <div className="relative h-screen flex flex-col lg:flex-row bg-gradient-to-r from-green-800 via-green-900 to-black">
-      <div className="flex flex-col justify-center px-6 sm:px-8 xs:px-8 lg:px-12 py-12 lg:w-1/2">
+    <div className="relative h-screen flex flex-col lg:flex-row bg-gradient-to-r from-green-800 via-green-900 to-black overflow-hidden">
+      <div className="flex flex-col justify-center px-6 sm:px-8 lg:px-12 py-12 lg:w-1/2">
         <div className="mx-auto max-w-lg lg:mx-0 space-y-6">
           <Logo />
           <Header />
