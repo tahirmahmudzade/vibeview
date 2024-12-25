a999709b06d98c081120ce57c9a3326f3a6416e0
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-12-25 11:35:20 +0100

    chore(image): configure multiple external image domains

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.scdn.co", pathname: "/image/**" },
+      { protocol: "https", hostname: "mosaic.scdn.co", pathname: "**" },
+      {
+        protocol: "https",
+        hostname: "image-cdn-fa.spotifycdn.com",
+        pathname: "/image/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-ak.spotifycdn.com",
+        pathname: "/image/**",
+      },
     ],
   },
 };
