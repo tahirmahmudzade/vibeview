c66e65ac9de781a51210bae5f8d0c11518031f0c
Tahir Mahmudzade <tahirmahmudzade25@gmail.com>
2024-12-26 15:54:55 +0100

    fix(prod): unable to find component related react issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,16 @@ import Logo from "@/components/Logo";
 import Header from "@/components/Header";
 import ButtonActions from "@/components/ButtonActions";
 import HeroImage from "@/components/HeroImage";
-import InstallAlert from "@/components/InstallAlert";
+import InstallAlertClient from "@/components/InstallAlertClient";
 
 export default function Home() {
   return (
     <div className="relative h-screen flex flex-col lg:flex-row bg-gradient-to-r from-green-800 via-green-900 to-black overflow-hidden">
       {/* Install Alert */}
-      <InstallAlert />
-
+      <InstallAlertClient />
       {/* Left Section */}
       <div className="flex flex-col justify-center px-6 sm:px-8 lg:px-12 py-12 lg:w-1/2">
         <div className="mx-auto max-w-lg lg:mx-0 space-y-6">
-          <div className="space-y-2">
-            <Logo />
-            <Header />
-          </div>
           <div className="space-y-2">
             <Logo />
             <Header />
